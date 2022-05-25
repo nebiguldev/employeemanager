07cb6d7abbc3ebc7db9853842d399f030a141674
Nebi GUL <nebi.gul@etiya.com>
2022-05-25 23:38:39 +0300

    Crud Operation with angular

---
+++ src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepo.java
@@ -0,0 +1,13 @@
+package tech.getarrays.employeemanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.getarrays.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
